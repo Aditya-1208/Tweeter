ef281239ebbb0128904ff66894e03bf9cfb93b57
Aditya <adityaagr.201ec103@gmail.com>
2022-11-12 18:58:49 +0530

    user model modified, added validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
+
+  validates :email, presence: true, format: {with: /\A[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\z/, message: "Must be a valid email address"}
 end
