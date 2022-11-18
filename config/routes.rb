87be7af58d32d7be60a8ae55a7f663c42e04eb4b
Aditya <adityaagr.201ec103@gmail.com>
2022-11-18 11:54:59 +0530

    password update feature and using form object errors

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "main#index"
+
   get "about-us", to: "about#index", as: "about"
+
   get "signup", to: "registrations#new"
   post "signup", to: "registrations#create"
+
   get "login", to: "sessions#new"
   post "login", to: "sessions#create"
   delete "logout", to: "sessions#destroy"
+
+  get "password", to: "passwords#edit", as: "edit_password"
+  patch "password", to: "passwords#update"
 end
