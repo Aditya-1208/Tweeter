344b109a1668a5435fff5f2668dfdc03a8ade0d0
Aditya <adityaagr.201ec103@gmail.com>
2022-11-19 20:36:50 +0530

    twitter account model, storing user data on successfull connection

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   has_secure_password
-
+  has_many :twitter_accounts
   validates :email, presence: true, format: {with: /\A[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+\z/, message: "Must be a valid email address"}
 end
