50528fe20d4b2331303282f96f86c15d92b310f8
Aditya <adityaagr.201ec103@gmail.com>
2022-11-19 13:52:37 +0530

    password reset, update and edit actions

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -13,4 +13,29 @@ class PasswordResetsController < ApplicationController
       redirect_to root_path, notice: "If that email exists in our database, you would've got a password reset link!"
 
   end
+
+  def edit
+    @user = User.find_signed!(params[:token],purpose: "password reset")
+    # binding.irb
+  rescue ActiveSupport::MessageVerifier::InvalidSignature
+    redirect_to login_path, alert: "Token Expired, Please try again!"
+
+  end
+
+  def update
+    @user = User.find_signed!(params[:token], purpose: "password reset")
+    if @user.update(password_params)
+      redirect_to login_path, notice: "Password reset successfully!"
+    else
+      render :edit
+
+    end
+  end
+
+  private
+
+  def password_params
+    params.require(:user).permit(:password,:password_confirmation)
+  end
+
 end
