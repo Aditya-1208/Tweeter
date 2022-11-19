fb83152488f7508e44145a18a4fbb08094cd8914
Aditya <adityaagr.201ec103@gmail.com>
2022-11-19 17:25:23 +0530

    added oauth gem, twitter app authorization and callback route

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,5 @@
+# config/initializers/omniauth.rb
+Rails.application.config.middleware.use OmniAuth::Builder do
+  # provider :twitter2, ENV["TWITTER_CLIENT_ID"], ENV["TWITTER_CLIENT_SECRET"], callback_path: '/auth/twitter2/callback', scope: "tweet.read users.read"
+  provider :twitter, Rails.application.credentials.dig(:twitter,:api_key), Rails.application.credentials.dig(:twitter,:api_secret)
+end
