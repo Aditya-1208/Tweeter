582785a9c019646bdf94ecdb900a6d7db9cad7f8
Aditya <adityaagr.201ec103@gmail.com>
2022-11-19 21:44:03 +0530

    showing and disconnecting twitter accounts

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ class OmniauthCallbacksController < ApplicationController
       token: auth.credentials.token,
       secret: auth.credentials.secret
     )
-    redirect_to root_path, notice: "Successfully connected account!"
+    redirect_to twitter_accounts_path, notice: "Successfully connected account!"
   end
 
   def auth
