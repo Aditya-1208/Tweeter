dd6717fe8c89d1d26cb962884ff11c1bcc7b62c7
Aditya <adityaagr.201ec103@gmail.com>
2022-11-20 13:39:57 +0530

    using active jobs to schedule tweets

---
+++ app/jobs/tweet_job.rb
@@ -0,0 +1,10 @@
+class TweetJob < ApplicationJob
+  queue_as :default
+
+  def perform(tweet)
+    # many updates on tweet => schedule multiple jobs, but we'll run only latest time
+    binding.irb
+    return if (tweet.published? || tweet.publish_at > Time.current)
+    tweet.publish_to_twitter!
+  end
+end
